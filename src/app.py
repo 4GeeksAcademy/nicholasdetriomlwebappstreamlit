38239777b2fa65f6ebcaea2c2ecd204d46142ccb
Nicholas Detrio <56412393+nicholasdetrio@users.noreply.github.com>
2024-04-18 00:17:59 +0000

    second commit with cache

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 import joblib
 import streamlit as st
 import pandas as pd
- 
-model = joblib.load("trained_model.joblib")
-print(type(model))
 
+@st.cache_resource
+def load_model():
+    return joblib.load("trained_model.joblib")
+
+model = load_model()
+#print(type(model))
+
+@st.cache_data
 def preprocess_input(val1, val2, val3, val4, val5):
     # Create a dictionary from the input values
     input_dict = {
@@ -14,13 +19,11 @@ def preprocess_input(val1, val2, val3, val4, val5):
     "gender": [val4],
     "lunch": [val5]
     }
-    print(input_dict)
+    # print(input_dict)
     
     # Convert dictionary to DataFrame
     input_df = pd.DataFrame(input_dict)
-    
-
-    
+        
     return input_df
 
 st.title("Student Test Scores Predictor")
@@ -45,11 +48,11 @@ val5 = st.selectbox(
     'lunch', 
     ('Does not qualify', 'Qualifies for reduced/free lunch')
 )
-print("val1:", val1)
-print("val2:", val2)
-print("val3:", val3)
-print("val4:", val4)
-print("val5:", val5)
+# print("val1:", val1)
+# print("val2:", val2)
+# print("val3:", val3)
+# print("val4:", val4)
+# print("val5:", val5)
 
 if st.button("Predict"):
     # Preprocess the input data
